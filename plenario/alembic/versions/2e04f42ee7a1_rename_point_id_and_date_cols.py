4b9bf045513e739e7fad9b0b09c9391f7eb0db54
Will Engler <Engler.Will@gmail.com>
2015-11-19 11:27:54 -0600

    Standardize id and date names in point datasets

---
+++ plenario/alembic/versions/2e04f42ee7a1_rename_point_id_and_date_cols.py
@@ -0,0 +1,54 @@
+"""rename_point_id_and_date
+
+Revision ID: 4e960796230e
+Revises:
+Create Date: 2015-11-19 10:28:48.518544
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '4e960796230e'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+from alembic import op
+from plenario.models import MetaTable
+from plenario.database import session
+
+
+def dataset_names_with_date_col_names():
+    return [(row.dataset_name, row.observed_date)
+            for row in session.query(MetaTable.dataset_name, MetaTable.observed_date).all()]
+
+
+def upgrade():
+    # Make foo_row_id -> point_id for all point datasets foo
+    for dataset_name, date_col in dataset_names_with_date_col_names():
+        table_name = 'dat_' + dataset_name
+        op.alter_column(
+            table_name,
+            '{}_row_id'.format(dataset_name),
+            new_column_name='point_id'
+        )
+        op.alter_column(
+            table_name,
+            date_col,
+            new_column_name='point_date'
+        )
+
+
+def downgrade():
+    # Make point_id -> foo_row_id for all point datasets foo
+    for dataset_name, date_col in dataset_names_with_date_col_names():
+        table_name = 'dat_' + dataset_name
+        op.alter_column(
+            table_name,
+            'point_id',
+            new_column_name='{}_row_id'.format(dataset_name)
+        )
+        op.alter_column(
+            table_name,
+            'point_date',
+            new_column_name=date_col
+        )
